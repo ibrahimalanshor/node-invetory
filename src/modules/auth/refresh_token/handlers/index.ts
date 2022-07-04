03a5bce741c462e0682d2dfd64338610b371aaf4
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-07-04 19:49:08 +0700

    fix refresh token

--- src/modules/auth/refresh_token/handlers/index.ts
+++ src/modules/auth/refresh_token/handlers/index.ts
@@ -1,1 +1,1 @@
-export { default as jwtSequelizeRefreshTokenHandle } from './jwt_sequelize_refresh_token.handle';
+export { default as jwtSequelizeRefreshToken } from './jwt_sequelize_refresh_token.handle';
